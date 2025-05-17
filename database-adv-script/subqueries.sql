8104b1908dcfc48efc3495376b1604d3a5ce228d
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-17 23:48:21 +0300

    col-query

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,1 +1,4 @@
-SELECT * FROM review INNER JOIN property on review.property_id = p.property_id WHERE rating > 4.0;
+SELECT * FROM property WHERE property_id IN (SELECT property_id FROM review GROUP BY property_id HAVING AVG(rating) > 4.0);
+
+
+SELECT * FROM user WHERE (SELECT COUNT(*) FROM booking WHERE booking.user_id = user.user_id) > 3;
