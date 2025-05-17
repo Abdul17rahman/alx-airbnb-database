125b2af000f0a49876c2b6a7c7dd2bae4edec1b7
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-17 12:11:32 +0300

    adv query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,5 @@
 SELECT * FROM booking INNER JOIN user on booking.user_id = user.user_id;
 
-SELECT * FROM property LEFT JOIN review on review.property_id = property.property_id;
+SELECT * FROM property LEFT JOIN review on review.property_id = property.property_id ORDER BY created_at;
 
 SELECT * FROM user FULL OUTER JOIN booking on booking.user_id = user.user_id;
