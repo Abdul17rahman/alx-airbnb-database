49c0d15b7424cb8dbc6d3ad2e623c0f0505c1f2f
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-17 12:09:54 +0300

    adv query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -1,5 +1,5 @@
-select * from booking inner join user on booking.user_id = user.user_id;
+SELECT * FROM booking INNER JOIN user on booking.user_id = user.user_id;
 
-select * from property left join review on review.property_id = property.property_id;
+SELECT * FROM property LEFT JOIN review on review.property_id = property.property_id;
 
-select * from user full join booking on booking.user_id = user.user_id;
+SELECT * FROM user FULL OUTER JOIN booking on booking.user_id = user.user_id;
