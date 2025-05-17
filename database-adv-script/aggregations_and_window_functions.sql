d60b1ef956768804ef9903c6dcf7ed8299e56f38
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-18 00:01:47 +0300

    rank

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,9 @@
+SELECT count(*) FROM booking GROUP BY booking.user_id;
+
+SELECT 
+    *
+    COUNT(booking.booking_id),
+    RANK() OVER (ORDER BY COUNT(booking.booking_id) DESC)
+FROM property
+LEFT JOIN booking ON property.property_id = booking.property_id
+GROUP BY property.property_id, property.name;
