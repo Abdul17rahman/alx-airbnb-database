073dd247c892988345af4a757db9674680be07aa
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-18 00:34:56 +0300

    performance and indexing

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,5 @@
-CREATE INDEX user_id_idx on user(user_id);
+CREATE INDEX user_id_idx ON user(user_id);
 
-CREATE INDEX booking_id_idx on booking(booking_id);
+CREATE INDEX booking_id_idx ON booking(booking_id);
 
-CREATE INDEX property_id_idx on property(property_id);
+CREATE INDEX property_id_idx ON property(property_id);
