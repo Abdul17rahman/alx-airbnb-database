97cd739c64a66391ec3179a5f54c14b1b33c9fa9
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-18 12:43:20 +0300

    perfomance complex queries

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -1,5 +1,5 @@
 EXPLAIN ANALYZE
-SELECT * FROM booking INNER JOIN user ON booking.user_id = user.user_id INNER JOIN property ON booking.property_id = property.property_id INNER JOIN payment ON payment.booking_id = booking.booking_id;
+SELECT * FROM booking INNER JOIN user ON booking.user_id = user.user_id INNER JOIN property ON booking.property_id = property.property_id INNER JOIN payment ON payment.booking_id = booking.booking_id WHERE payment.amount > 100 AND property.location = 'New York';
 
 CREATE INDEX booking_user_id_idx ON booking(user_id);
 CREATE INDEX booking_property_id_idx ON booking(property_id);
