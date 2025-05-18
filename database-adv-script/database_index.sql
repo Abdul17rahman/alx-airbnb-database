cc59cbe14a20c8f4a763d56e71c3c8278c061a44
Abdul Rahman <abdulnsamba@gmail.com>
2025-05-18 11:11:59 +0300

    performance and indexing fix

--- database-adv-script/database_index.sql
+++ database-adv-script/database_index.sql
@@ -1,5 +1,19 @@
+```
+This is before the indexing.
+```
+
+EXPLAIN ANALYZE
+SELECT first_name FROM user INNER JOIN booking on booking.user_id = user.user_id; 
+
 CREATE INDEX user_id_idx ON user(user_id);
 
 CREATE INDEX booking_id_idx ON booking(booking_id);
 
 CREATE INDEX property_id_idx ON property(property_id);
+
+```
+This is after the index
+```
+
+EXPLAIN ANALYZE
+SELECT first_name FROM user INNER JOIN booking on booking.user_id = user.user_id;
